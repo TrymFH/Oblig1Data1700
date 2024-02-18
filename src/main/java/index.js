797ffe37b558115b97992c1038a2412d23458a09
TrymFH <trym.fjosne.hexeberg@gmail.com>
2024-02-18 20:30:32 +0100

    Opprettet bilett objektet og linket det til verdier som blir valgt.

--- src/main/java/index.js
+++ src/main/java/index.js
@@ -1,3 +1,23 @@
-function velg (){
+let billet = {
+    film: getElement.By.Id("film").value,
+    antall: getElement.By.Id("antall").value,
+    fornavn: getElement.By.Id("fornavn").value,
+    etternavn: getElement.By.Id("etternavn").value,
+    telefonnr: getElement.By.Id("telefonnr").value,
+    epost: getElement.By.Id("film").value
+}
+
+function velgFilm (){
     alert("Valgte" + getElementById("Velg film").value);
+}
+
+function sjekk () {
+    const streng = document.getElementById("antall").value;
+    const tall = Number(streng);
+    if(isNaN(antall)) {
+        alert("Ikke et tall!");
+    } else {
+        alert("Tallet er :"+antall);
+    }
+
 }
